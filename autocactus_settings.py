1007b6eb611d59b9f1e02a6da98d4f597dbbf171
Mariusz Hruszowiec <mariusz.hruszowiec@pwr.edu.pl>
2017-03-11 15:44:05 +0100

    Settings added, checker skeleton added.

---
+++ autocactus_settings.py
@@ -0,0 +1,23 @@
+EMAIL_WHITELIST = [
+	"firstemail@mail.com",
+	"secondauthor@gmail.com"
+]
+
+# main inbox settings
+INBOX_NAME = ''
+INBOX_PASSWORD = ''
+INBOX_MAILSERVER = ''
+
+# MongoDB settings
+MONGODB_SERVER = ''
+MONGODB_USER = ''
+MONGODB_PASSWORD = ''
+
+# publisher checking settings
+# in seconds
+PUBLISHER_CHECK_INTERVAL = 3600
+
+# checker interval settings
+# in seconds
+# do not know if it is needed
+CHECKER_CHECK_INTERVAL = 3600
--- checker.py
+++ checker.py
@@ -1,1 +1,19 @@
-# -*- coding: utf-8 -*-
+import imaplib
+import autocactus_settings as aset
+
+
+def check():
+    """
+    Checker main function.
+    """
+    imap_client = imaplib.IMAP4(aset.INBOX_MAILSERVER)
+    imap_client.login(aset.INBOX_NAME, aset.INBOX_PASSWORD)
+    # select INBOX - default
+    imap_client.select()
+    typ, data = imap_client.search(None, 'ALL')
+    for num in data[0].split():
+        typ, data = imap_client.fetch(num, '(RFC822)')
+        # process message
+
+    imap_client.close()
+    imap_client.logout()
